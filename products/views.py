101f8c52567d6144cc98e6d45d0784a2e7a29469
collinearen <118291046+collinearen@users.noreply.github.com>
2023-05-15 01:08:14 +0300

    Pagination

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render, HttpResponseRedirect
 
 from products.models import Product, ProductCategory, Basket
@@ -10,12 +11,28 @@ def index(request):
     return render(request=request, template_name='products/index.html', context=context)
 
 
-def products(request):
-    context = {'title': 'Ruffle',
-               'products': Product.objects.all(),
-               'categories': ProductCategory.objects.all()
-               }
-    return render(request=request, template_name='products/products.html', context=context)
+def products(request, category_id=None):
+    per_page = 3
+    if category_id:
+        category = ProductCategory.objects.get(id=category_id)
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+    page = request.GET.get('page', 1)
+    if not isinstance(page, int):
+        if page.isdigit():
+            page = int(page)
+        else:
+            page = 1
+    paginator = Paginator(products, per_page=per_page)
+    page_products = paginator.page(page)
+    return render(request, 'products/products.html', context={
+        'title': 'Ruffle продукты',
+        'products': page_products,
+        'category': ProductCategory.objects.all(),
+        'category_id': category_id
+    })
+
 
 
 @login_required
